dc9dbdba41db1d79e35cab22baa5177e3947c85d
wlzh123 <593942008@qq.com>
2018-02-03 20:45:51 +0800

    Create partition.cpp

---
+++ partition.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void sw(int *a,int *b){
+    int temp=*a;
+
+}
+int partiton(int ar[],int first,int last){
+    int temp=ar[first];
+    while(first<last){
+        while(ar[last]>temp&&first<last){
+            last--;
+        }
+        ar[first]=ar[last];
+        while(ar[first]<=temp&&first<last){
+            first++;
+        }
+        ar[last]=ar[first];
+    }
+    ar[first]=temp;
+    return first;
+}
+int main(){
+    int arr[]={3,2,3,4,5,6,7,8};
+    int res= partiton(arr,0,8);
+    cout<<res<<endl;
+    for(int i=0;i<8;i++)
+        cout<<arr[i]<<" ";
+}
